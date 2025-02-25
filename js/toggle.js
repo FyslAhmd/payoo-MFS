d49631365c06537a6852241112ada5c19b7ec34c
Faysal Ahmed <info.faysal.32@gmail.com>
2025-02-25 20:18:23 +0600

    project complete

--- js/toggle.js
+++ js/toggle.js
@@ -1,11 +1,20 @@
 document.getElementById('cash-out').style.display = 'none';
+document.getElementById('transaction-history').style.display = 'none';
 
 document.getElementById('add-money-box').addEventListener('click', function() {
-    document.getElementById('add-money').style.display = 'block';
-    document.getElementById('cash-out').style.display = 'none';
+    handleToggle('add-money', 'block');
+    handleToggle('cash-out', 'none');
+    handleToggle('transaction-history', 'none');
 })
 
 document.getElementById('cash-out-box').addEventListener('click', function() {
-    document.getElementById('add-money').style.display = 'none';
-    document.getElementById('cash-out').style.display = 'block';
+    handleToggle('add-money', 'none');
+    handleToggle('cash-out', 'block');
+    handleToggle('transaction-history', 'none');
 })
+
+document.getElementById("transaction-history-box").addEventListener("click", function() {
+    handleToggle('add-money', 'none');
+    handleToggle('cash-out', 'none');
+    handleToggle('transaction-history', 'block');
+});
